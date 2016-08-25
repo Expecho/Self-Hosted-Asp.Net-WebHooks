7411d9f42e3ed05c8fd075d76a6b36416eeebdae
Peter Bons <github@2000miles.nl>
2016-08-25 14:59:06 +0200

    Added some code documentation

--- Receiver/Program.cs
+++ Receiver/Program.cs
@@ -21,9 +21,11 @@ namespace Receiver
             using (WebApp.Start<Startup>(webhookReceiverBaseAddress))
             using (var httpClient = new HttpClient(handler))
             {
-                Console.WriteLine("Press any key to register");
+                // User should wait until the server (WebApiHost) is up and running before registering
+                Console.WriteLine("Press any key to register"); 
                 Console.ReadKey();
 
+                // Create a webhook registration
                 var registration = new Registration
                 {
                     WebHookUri = $"{webhookReceiverBaseAddress}/api/webhooks/incoming/custom",
@@ -34,28 +36,30 @@ namespace Receiver
                     Filters = new List<string> { "MessagePostedEvent" } 
                 };
 
-                var result = httpClient.PostAsJsonAsync($"http://{Environment.MachineName}.deheer-groep.nl:9000/api/webhooks/registrations", registration).Result;
+                // Register our webhook
+                var result = httpClient.PostAsJsonAsync($"{webhookSenderBaseAddress}/api/webhooks/registrations", registration).Result;
                 Console.WriteLine(result.IsSuccessStatusCode ? "Registration succesful" : "Registration failed");
 
                 Console.WriteLine("Press 'm' to send a message");
                 Console.WriteLine("Press 'r' to remove a message");
                 Console.WriteLine("Press 'q' to quit");
 
-                ConsoleKey key = ConsoleKey.Spacebar;
+                ConsoleKey key;
 
-                while (key != ConsoleKey.Q)
+                do
                 {
                     key = Console.ReadKey().Key;
                     switch (key)
                     {
                         case ConsoleKey.M:
-                            httpClient.PostAsJsonAsync($"{webhookSenderBaseAddress}/api/messages", new Message { Sender = "Receiver", Body = "Hello From Receiver" }).Wait();
+                            httpClient.PostAsJsonAsync($"{webhookSenderBaseAddress}/api/messages",
+                                new Message {Sender = "Receiver", Body = "Hello From Receiver"}).Wait();
                             break;
                         case ConsoleKey.R:
                             httpClient.DeleteAsync($"{webhookSenderBaseAddress}/api/messages?id=1").Wait();
                             break;
                     }
-                }
+                } while (key != ConsoleKey.Q);
             }
         }
     }
--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -21,9 +21,12 @@ namespace WebApiHost
             using (var client = new HttpClient(handler))
             {
                 Console.WriteLine("Server up and running.");
+
+                // User should wait until the webhook receiver has completed the registration.
                 Console.WriteLine("Press any key to send a message");
                 Console.ReadKey();
 
+                // trigger a webhook call
                 client.PostAsJsonAsync($"{webHookSenderBaseAddress}/api/messages", new Message { Sender = "WebApiHost", Body = "Hello From Sender" }).Wait();
                 
                 Console.WriteLine("Press any key to exit");
